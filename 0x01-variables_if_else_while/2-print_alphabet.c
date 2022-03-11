18f244df62e108387358de0927d1b3037d11bd02
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-03-11 12:05:19 -0800

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
-	char a;
+	char c;
 
-	for (a = 'a'; a <= 'z'; a++)
-		putchar(a);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
 
 return (0);
 }
