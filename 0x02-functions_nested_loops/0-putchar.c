0b0cf9822327efd231004c3471f0ed7c3bd79ba3
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-03-15 13:45:51 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,47 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_times_table - prints the n times table, starting with 0
+ * n: size of time table
  */
-int _putchar(char c)
+void print_times_table(int n)
 {
-	return (write(1, &c, 1));
+ int a,b,c
+
+ if (n >= 0 && n <= 15)
+ {
+  for (a = 0; a <= n; a++)
+  {
+   for (b = 0; b <= n; b++)
+   {
+    c = a * b;
+    if (b == 0)
+    {
+     _putchar(c + '0');
+    } else if (c < 10 && b != 0)
+    {
+     _putchar(',');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar(c + '0');
+    } else if (c >= 10 && c < 100)
+    {
+     _putchar(',');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar((c / 10) + '0');
+     _putchar((c % 10) + '0');
+    } else if (c >= 100)
+    {
+     _putchar(',');
+     _putchar(' ');
+     _putchar((c / 100) + '0');
+     _putchar(((c / 10) % 10) + '0');
+     _putchar((c % 10) + '0');
+    }
+   }
+   _putchar('\n');
+  }
+ }
 }
