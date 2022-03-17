772168ddadff05be644ebfcc14859844c95e51e3
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-03-17 13:24:51 -0700

    more nested lop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,22 +2,15 @@
 #include <stdio.h>
 
 /**
-* _isupper - Shows 1 if the input is a
-* uppercase character. Another cases, shows 0
-*
-* @c: The character in ASCII code
-*
-* Return: 1 for uppercase. 0 for the rest.
-*/
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
-        if (c >= 65 && c <= 90)
-        {
-        return (1);
-        }
-        else
-        {
-        return (0);
-        }
-        _putchar('\n');
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
