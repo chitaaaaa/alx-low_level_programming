66310d5205a5d5fb659e543c8bf9151795aa0515
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-04-05 15:51:39 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
  * create_array - create array
  * @size: size
  * @c: constant
- * return: char pointer
+ * Return: char pointer
  */
 char *create_array(unsigned int size, char c)
 {
@@ -12,12 +12,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return(0);
+	return (0);
 	p = malloc((size) * sizeof(char));
 	if (p == '\0')
-		return (0);for (i = 0; i < size: i++)
-		{
-			p[1] = c;
-		}
-	return (p);
+	return (0);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+return (p);
 }
