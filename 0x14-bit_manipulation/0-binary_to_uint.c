a0a3f27772cf3edd31dba8097b8f86faae9909b5
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-04-28 13:07:17 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bit =0;
+	while (b && *b)
+	{
+		if (*b != '0' && *b != '1')
+			return(0);
+
+		bit = bit << 1;
+		bit = bit | (*b - '0');
+		b++;
+	}
+	return (bit);
+}
