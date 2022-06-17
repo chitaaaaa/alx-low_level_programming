9c4ae0298f7149e6cb1ba78a461c8b82e40a38fd
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-06-16 22:42:59 -0700

    dynamic libraries

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,14 +7,14 @@ void print_times_table(int n)
 {
   int i, j, k;
 
-  if (n > 15 || n < 0)
-    return;
-  for (i = 0; i <= n; i++)
-  {
-    for (j = 0; j <= n; j++)
-    {
-      k = i * j;
-      if (j == 0)
+	  if (n > 15 || n < 0)
+    		return;
+  	for (i = 0; i <= n; i++)
+  	{
+    	for (j = 0; j <= n; j++)
+    	{
+      		k = i * j;
+     	 if (j == 0)
       {
         _putchar(k + '0');
       }
