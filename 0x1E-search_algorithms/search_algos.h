ba15ccd6bacb2ca48001499c5bb2f17baaae978c
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-10-05 17:59:50 -0700

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,21 @@
 #include <stdlib.h>
 #include <math.h>
 
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t r);
 int binary_search(int *array, size_t size, int value);
